bc6fbdb84b27ae5cc1968810e1d9c07fb28e6093
Eduardo Augusto Lima Pereira <42621512+Edd002@users.noreply.github.com>
2025-12-14 14:53:27 -0300

    #Fase-4-ajuste-time-zone : Ajuste de time zone para São Paulo e formatação de AverageAssessmentQuantitiesByDay para duas casas decimais

--- src/main/java/fiap/tech/challenge/online/course/notification/serverless/payload/record/WeeklyEmailNotificationResponse.java
+++ src/main/java/fiap/tech/challenge/online/course/notification/serverless/payload/record/WeeklyEmailNotificationResponse.java
@@ -1,10 +1,11 @@
 package fiap.tech.challenge.online.course.notification.serverless.payload.record;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 public record WeeklyEmailNotificationResponse(AdministratorResponse administrator, Long urgentAssessmentQuantity, Double averageAssessmentScore, List<AssessmentQuantityByDayResponse> assessmentQuantitiesByDay) {
 
     public Double getAverageAssessmentQuantitiesByDay() {
-        return assessmentQuantitiesByDay.stream().mapToDouble(AssessmentQuantityByDayResponse::assessmentQuantity).average().orElse(0.0);
+        return Double.parseDouble(new DecimalFormat("#.##").format(assessmentQuantitiesByDay.stream().mapToDouble(AssessmentQuantityByDayResponse::assessmentQuantity).average().orElse(0.0)));
     }
 }
